bbf6ceea489a728f415afe243965142a051d950e
HienNguyen102 <nguyenconghien0102@gmail.com>
2017-03-02 08:59:37 -0800

    - Add file debug launch - Add function get users have check token

--- routes/users.js
+++ routes/users.js
@@ -10,18 +10,29 @@ module.exports = router;*/
 var express = require('express'),
     _       = require('lodash'),
     config  = require('../config'),
-    jwt     = require('jsonwebtoken')
+    jwt     = require('jsonwebtoken'),
+    express_jwt     = require('express-jwt'),
     db      = require('../db');
 var app = module.exports = express.Router();
 var secretKey = "don't share this key";
 function createToken(user) {
   return jwt.sign(_.omit(user, 'password'), config.secretKey, { expiresIn: 60*60*5 });
 }
+var jwtCheck = express_jwt({
+  secret: config.secretKey
+});
 function getUserDB(username, done) {
   db.get().query('SELECT * FROM gcm_users WHERE username = ? LIMIT 1', [username], function(err, rows, fields) {
     if (err) throw err;
     done(rows[0]);
   });
+}
+function getUsersDB(user_id, done){
+  //console.log(user_id);
+    db.get().query('SELECT * FROM gcm_users WHERE NOT (id = ?)', user_id, function(err, rows) {
+        if (err) throw err;
+        done(rows);
+    });
 }
 app.post('/api/user/create', function(req, res) {  
   if (!req.body.username || !req.body.password) {
@@ -73,5 +84,17 @@ app.get('/user/check/:username', function(req, res) {
   getUserDB(req.params.username, function(user){
     if(!user) res.status(201).send({username: "OK"});
     else res.status(400).send("A user with that username already exists");
+  });
+});
+
+app.use('/api/user/get_users', jwtCheck);
+
+app.get('/api/user/get_users/:user_id', function(req, res) {
+  console.log(req.params.user_id);
+  if (!req.params.user_id) {
+    return res.status(400).send("You must send a user id");
+  }
+  getUsersDB(req.params.user_id ,function(result) {
+      res.status(200).send(result);
   });
 });
