42c71fecd146f0f6f4deeaaa6bd988b34db2e6f0
HienNguyen102 <nguyenconghien0102@gmail.com>
2017-03-05 03:44:58 -0800

    Get users in DB

--- socket.js
+++ socket.js
@@ -1,7 +1,35 @@
 exports.connect = function(server){
     var io = require('socket.io')(server);
+    var db = require('./db');
     var userList = [];
     var typingUsers = [];
+    function updateUserInDB(user_id, socketId ,is_online, done){
+        var strQuery = "UPDATE gcm_users SET isOnline = ";
+        if (is_online){
+            strQuery += "1, ";
+        }else{
+            strQuery += "0, ";
+        }
+        strQuery += "socket_id = '"+socketId+"' WHERE id = " + user_id;
+        db.get().query(strQuery, function(err, rows){
+            if (err) throw err;
+            done(rows);
+        });
+    }
+    //When disconnect
+    function updateUserInDBWhenDisconnect(socketId, done){
+        var strQuery = "UPDATE gcm_users SET isOnline = 0 WHERE socket_id = '" + socketId +"'";
+        db.get().query(strQuery, function(err, rows){
+            if (err) throw err;
+            done(rows);
+        });
+    }
+    function getAllUsersInDB(done){
+        db.get().query('SELECT * FROM gcm_users', function(err, rows) {
+            if (err) throw err;
+            done(rows);
+        });
+    }
 
     io.on('connection', function(clientSocket) {
         console.log('user '+clientSocket.id+' connected');
@@ -9,7 +37,7 @@ exports.connect = function(server){
         clientSocket.on('disconnect', function(){
             console.log('user '+clientSocket.id+' disconnected');
 
-            var clientNickname;
+            /*var clientNickname;
             for (var i=0; i<userList.length; i++) {
             if (userList[i]["id"] == clientSocket.id) {
                 userList[i]["isConnected"] = false;
@@ -20,21 +48,29 @@ exports.connect = function(server){
 
             delete typingUsers[clientNickname];
             io.emit("userList", userList);
-            //io.emit("userExitUpdate", clientNickname); //Hien tam comment
-            //io.emit("userTypingUpdate", typingUsers); //Hien tam comment
+            io.emit("userExitUpdate", clientNickname);
+            io.emit("userTypingUpdate", typingUsers);*/
+            updateUserInDBWhenDisconnect(clientSocket.id, function(result){
+                console.log("update success when disconnect");
+            });
         });
 
 
-        clientSocket.on("exitUser", function(clientNickname){
+        clientSocket.on("exitUser", function(clientId, clientNickname){
             var message = "User " + clientNickname + " was disconnected.";
             console.log(message);
-            for (var i=0; i<userList.length; i++) {
+            /*for (var i=0; i<userList.length; i++) {
             if (userList[i]["id"] == clientSocket.id) {
                 userList.splice(i, 1);
                 break;
             }
             }
-            io.emit("userExitUpdate", clientNickname);
+            io.emit("userExitUpdate", clientNickname);*/
+            updateUserInDB(clientId, clientSocket.id, false, function(result){
+                getAllUsersInDB(function(resultSelect){
+                    io.emit("userList", resultSelect);
+                });
+            });
         });
 
 
@@ -46,11 +82,11 @@ exports.connect = function(server){
         });
 
 
-        clientSocket.on("connectUser", function(clientNickname) {
+        clientSocket.on("connectUser", function(clientId, clientNickname) {
             var message = "User " + clientNickname + " was connected.";
             console.log(message);
 
-            var userInfo = {};
+            /*var userInfo = {};
             var foundUser = false;
             for (var i=0; i<userList.length; i++) {
                 if (userList[i]["nickname"] == clientNickname) {
@@ -70,7 +106,12 @@ exports.connect = function(server){
             }
 
             io.emit("userList", userList);
-            io.emit("userConnectUpdate", userInfo)
+            io.emit("userConnectUpdate", userInfo)*/
+            updateUserInDB(clientId, clientSocket.id, true, function(result){
+                getAllUsersInDB(function(resultSelect){
+                    io.emit("userList", resultSelect);
+                });
+            });
         });
 
 
