f0d0d2ba88b00736f0691dad7691762fe821ffa8
HienNguyen102 <nguyenconghien0102@gmail.com>
2017-03-11 08:35:43 -0800

    - Add room chat - Get room chat

--- routes/chat.js
+++ routes/chat.js
@@ -16,75 +16,87 @@ function getConversationReplyDB(c_id, done) {
   });
 }
 function getRoom(room_id, done){
-    //var room_number = Number(room_id);
     db.get().query('SELECT * FROM room WHERE id = ? LIMIT 1', [room_id], function(err, rows, fields){
         if (err) throw err;
         done(rows[0]);
     });
 }
+function getAllRooms(done){
+  db.get().query('SELECT * FROM room', function(err, rows){
+    if (err) throw err;
+    done(rows);
+  });
+}
+function getAllMessages(room_id, done){
+  db.get().query('SELECT * FROM message WHERE room_id = ?',[room_id], function(err, rows){
+    if (err) throw err;
+    done(rows);
+  });
+}
 
-app.post('/api/chat/create', function(req, res) {  
-  //req.body.user_one la nguoi gui tin
-  //req.body.room_id
-  //req.body.room_name
-  if (!req.body.room_id) {
+app.post('/api/chat/create', function(req, res) {
+  /*if (!req.body.room_id) {
     return res.status(400).send("Not enough info for create a room");
-  }
+  }*/
+  var currentTime = new Date();
   getRoom(req.body.room_id, function(room){
     if(!room){
         room = {
-            name: req.body.room_name
+            name: req.body.room_name,
+            latest_message: req.body.text,
+            latest_time: currentTime.getTime()
         };
         db.get().query('INSERT INTO room SET ?', [room], function(err, result){
-            if (err) throw err;
-            res.status(200).send(result);
+          if (err) throw err;
+          var arrUser = JSON.parse(req.body.users);
+          for (var i=0; i< arrUser.length; i++){
+              console.log(arrUser[i]);
+              db.get().query('INSERT INTO logged_in_user SET room_id = ?, user_id = ?', [result.insertId, arrUser[i]], function(err, resultSub1){
+                  if (err) throw err;
+              });
+          }
+          var message = {
+            room_id: result.insertId,
+            text: req.body.text,
+            user_id: arrUser[0],
+            to_user_id: arrUser[1],
+            time_stamp: currentTime.getTime()
+          };
+          db.get().query('INSERT INTO message SET ?', [message], function(err, resultSub2){
+            res.status(200).send(resultSub2);
+          });
         });
     }else{
-        //res.status(200).send(room);
-        var arrUser = JSON.parse(req.body.room_name);
-        for (var i=0; i< arrUser.length; i++){
-            console.log(arrUser[i]);
-             db.get().query('INSERT INTO logged_in_user SET room_id = ?, user_id = ?', [room.id, arrUser[i]], function(err, result){
-                if (err) throw err;
-                //res.status(200).send(result);
-            });
-        }
-        //res.status(200).send("ok");
+        var arrUser = JSON.parse(req.body.users);
+        var message = {
+          room_id: room.id,
+          text: req.body.text,
+          user_id: arrUser[0],
+          to_user_id: arrUser[1],
+          time_stamp: currentTime.getTime()
+        };
+        //Update latest_message of room
+        db.get().query("UPDATE room SET latest_message = ?, latest_time = ? WHERE id = ?", [req.body.text, currentTime.getTime(), room.id], function(err, result){
+          if (err) throw err;
+        });
+        db.get().query('INSERT INTO message SET ?', [message], function(err, result){
+          res.status(200).send(result);
+        });
     }
   });
-  /*getConversationDB(req.body.user_one, req.body.user_two, function(conversation){
-    if(!conversation) {
-      conversation = {
-        user_one: req.body.user_one,
-        user_two: req.body.user_two
-      };
-      db.get().query('INSERT INTO conversation SET ?', [conversation], function(err, result){
-        if (err) throw err;
-        conversation_reply = {
-          c_id_fk: result.insertId,
-          reply: req.body.reply,
-          user_id_fk: req.body.user_one
-        };
-        db.get().query('INSERT INTO conversation_reply SET ?', [conversation_reply], function(subErr, subResult){
-          if (subErr) throw subErr;
-        });
-        res.status(201).send({
-          msg : "Tao cuoc thoai thanh cong"
-        });
-      });
-    } else{//Da ton tai thi luu vao bang conversation_reply
-      conversation_reply = {
-          c_id_fk: conversation.c_id,
-          reply: req.body.reply,
-          user_id_fk: req.body.user_one
-        };
-      db.get().query('INSERT INTO conversation_reply SET ?', [conversation_reply], function(subErr, subResult){
-        if (subErr) throw subErr;
-        //res.status(200).send(subResult);
-        getConversationReplyDB(conversation.c_id, function(subResult2) {
-            res.status(200).send(subResult2);
-        });
-      });
-    }
-  });*/
+});
+
+app.get('/api/chat/rooms', function(req, res) {
+  getAllRooms(function(result) {
+      res.status(200).send(result);
+  });
+});
+
+app.get('/api/chat/get_messages/:room_id', function(req, res) {
+  if (!req.params.room_id) {
+    return res.status(400).send("You must send a user id");
+  }
+  getAllMessages(req.params.room_id ,function(result) {
+      res.status(200).send(result);
+  });
 });
