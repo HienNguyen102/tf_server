d75c08a31e131c52d1d42508fcf8bf691d4a089b
HienNguyen102 <nguyenconghien0102@gmail.com>
2017-03-16 08:39:47 -0700

    Add getGroups function

--- routes/chat.js
+++ routes/chat.js
@@ -3,36 +3,7 @@ var express = require('express'),
     jwt     = require('jsonwebtoken')
     db      = require('../db');
 var app = module.exports = express.Router();
-function getConversationDB(user_one, user_two, done) {
-  db.get().query('SELECT * FROM conversation WHERE (user_one = ? AND user_two = ?) OR (user_two = ? AND user_one = ?) LIMIT 1', [user_one, user_two, user_one, user_two], function(err, rows, fields) {
-    if (err) throw err;
-    done(rows[0]);
-  });
-}
-function getConversationReplyDB(c_id, done) {
-  db.get().query('SELECT * FROM conversation_reply WHERE c_id_fk = ?', [c_id], function(err, rows) {
-    if (err) throw err;
-    done(rows);
-  });
-}
 function getRoomId(user_one, user_two, done){
-  /*db.get().query('SELECT room_id FROM logged_in_user WHERE user_id = ?', [user_one], function(err, rows1, fields){
-    if (err) throw err;
-    //done(rows1);
-    for (i = 0; i < rows1.length; i++) {
-      db.get().query('SELECT room_id FROM logged_in_user WHERE room_id = ? AND user_id = ?', [rows1[i].room_id,user_two], function(err, rows2){
-        if (err) throw err;
-        if(rows2.length>0){
-          db.get().query('SELECT room_id FROM logged_in_user WHERE room_id = ?', [rows2[0].room_id], function(err, rows3){
-            if (err) throw err;
-            if(rows3.length==2){
-              done(rows3[0]);
-            }
-          });
-        }
-      });
-    }
-  });*/
   db.get().query('SELECT * FROM logged_in_user WHERE user_id = ? AND room_id IN(SELECT room_id FROM logged_in_user WHERE user_id = ?) AND room_id IN(select room_id from logged_in_user group by room_id HAVING count(room_id)=2)', [user_one, user_two], function(err, rows, fields){
     if(err) throw err;
     done(rows);
@@ -47,6 +18,12 @@ function getRoom(room_id, done){
 function getAllRooms(done){
   db.get().query('SELECT * FROM room', function(err, rows){
     if (err) throw err;
+    done(rows);
+  });
+}
+function getGroups(user_id, done){
+  db.get().query('select * FROM room WHERE id IN( SELECT room_id FROM logged_in_user WHERE user_id = ? AND room_id IN( select room_id from logged_in_user group by room_id HAVING count(room_id)>2))', [user_id], function(err, rows, fields){
+    if(err) throw err;
     done(rows);
   });
 }
@@ -125,6 +102,11 @@ app.get('/api/chat/get_messages/:room_id', function(req, res) {
 });
 app.get('/api/chat/get_room/:user_one/:user_two', function(req, res){
   getRoomId(req.params.user_one, req.params.user_two, function(result){
+    res.status(200).send(result[0]);
+  });
+});
+app.get('/api/chat/get_group/:user_id', function(req, res){
+  getGroups(req.params.user_id, function(result){
     res.status(200).send(result);
   });
 });
