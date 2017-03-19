31ff5572401a6691cc7cf1f825e9b498875490b4
HienNguyen102 <nguyenconghien0102@gmail.com>
2017-03-18 21:56:15 -0700

    - add field is_private - edit private chat

--- routes/chat.js
+++ routes/chat.js
@@ -3,8 +3,8 @@ var express = require('express'),
     jwt     = require('jsonwebtoken')
     db      = require('../db');
 var app = module.exports = express.Router();
-function getRoomId(user_one, user_two, done){
-  db.get().query('SELECT * FROM logged_in_user WHERE user_id = ? AND room_id IN(SELECT room_id FROM logged_in_user WHERE user_id = ?) AND room_id IN(select room_id from logged_in_user group by room_id HAVING count(room_id)=2)', [user_one, user_two], function(err, rows, fields){
+function getPrivateRoom(user_one, user_two, done){
+  db.get().query('SELECT id FROM room WHERE is_private = 1 AND id IN(SELECT room_id FROM logged_in_user WHERE user_id = ? AND room_id IN(SELECT room_id FROM logged_in_user WHERE user_id = ?))', [user_one, user_two], function(err, rows, fields){
     if(err) throw err;
     done(rows);
   });
@@ -32,7 +32,7 @@ function getAllRooms(done){
   });
 }
 function getGroups(user_id, done){
-  db.get().query('select * FROM room WHERE id IN( SELECT room_id FROM logged_in_user WHERE user_id = ? AND room_id IN( select room_id from logged_in_user group by room_id HAVING count(room_id)>2))', [user_id], function(err, rows, fields){
+  db.get().query('select * FROM room WHERE is_private = 0 AND id IN(SELECT room_id FROM logged_in_user WHERE user_id = ?)', [user_id], function(err, rows, fields){
     if(err) throw err;
     done(rows);
   });
@@ -44,58 +44,7 @@ function getAllMessages(room_id, done){
   });
 }
 
-app.post('/api/chat/create', function(req, res) {
-  /*if (!req.body.room_id) {
-    return res.status(400).send("Not enough info for create a room");
-  }*/
-  var currentTime = new Date();
-  getRoom(req.body.room_id, function(room){
-    if(!room){
-        room = {
-            name: req.body.room_name,
-            latest_message: req.body.text,
-            latest_time: currentTime.getTime()
-        };
-        db.get().query('INSERT INTO room SET ?', [room], function(err, result){
-          if (err) throw err;
-          var arrUser = JSON.parse(req.body.users);
-          for (var i=0; i< arrUser.length; i++){
-              console.log(arrUser[i]);
-              db.get().query('INSERT INTO logged_in_user SET room_id = ?, user_id = ?', [result.insertId, arrUser[i]], function(err, resultSub1){
-                  if (err) throw err;
-              });
-          }
-          var message = {
-            room_id: result.insertId,
-            text: req.body.text,
-            user_id: arrUser[0],
-            to_user_id: arrUser[1],
-            time_stamp: currentTime.getTime()
-          };
-          db.get().query('INSERT INTO message SET ?', [message], function(err, resultSub2){
-            res.status(200).send(resultSub2);
-          });
-        });
-    }else{
-        var arrUser = JSON.parse(req.body.users);
-        var message = {
-          room_id: room.id,
-          text: req.body.text,
-          user_id: arrUser[0],
-          to_user_id: arrUser[1],
-          time_stamp: currentTime.getTime()
-        };
-        //Update latest_message of room
-        db.get().query("UPDATE room SET latest_message = ?, latest_time = ? WHERE id = ?", [req.body.text, currentTime.getTime(), room.id], function(err, result){
-          if (err) throw err;
-        });
-        db.get().query('INSERT INTO message SET ?', [message], function(err, result){
-          res.status(200).send(result);
-        });
-    }
-  });
-});
-
+//**************Common**************
 app.get('/api/chat/rooms', function(req, res) {
   getAllRooms(function(result) {
       res.status(200).send(result);
@@ -110,11 +59,85 @@ app.get('/api/chat/get_messages/:room_id', function(req, res) {
       res.status(200).send(result);
   });
 });
+//***********************Common***********************
+
+//*******************Private chat*******************
+
+app.post('/api/chat/create_private_group', function(req, res) {
+  var currentTime = new Date();
+  var user_one_id = req.body.user_one_id;
+  var user_one_name = req.body.user_one_name;
+  var user_two_id = req.body.user_two_id;
+  var text = req.body.text;
+  if(!user_one_id || !user_one_name || !user_two_id || !text){
+    return res.status(400).send("Not enough info for create private chat");
+  }
+  var room = {
+      name: user_one_id + "_" + user_two_id,
+      created_by: user_one_id,
+      is_private: 1,
+      owner_name: user_one_name,
+      latest_message: text,
+      latest_time: currentTime.getTime()
+  };
+  db.get().query('INSERT INTO room SET ?', [room], function(err, result){
+    if (err) throw err;
+    var message = {
+      room_id: result.insertId,
+      text: text,
+      user_id: user_one_id,
+      to_user_id: user_two_id,
+      time_stamp: currentTime.getTime()
+    };
+    db.get().query('INSERT INTO message SET ?', [message], function(subErr, subRes){
+      if (subErr) throw subErr;
+    });
+    
+    db.get().query('INSERT INTO logged_in_user SET room_id = ?, user_id = ?', [result.insertId,user_one_id], function(subErr, resultSub1){
+        if (subErr) throw subErr;
+    });
+    db.get().query('INSERT INTO logged_in_user SET room_id = ?, user_id = ?', [result.insertId,user_two_id], function(subErr, resultSub1){
+        if (subErr) throw subErr;
+    });
+    res.status(200).send(result);
+  });
+});
+
+app.post('/api/chat/send_to_private_group', function(req, res){
+  var currentTime = new Date();
+  var room_id = req.body.room_id;
+  var user_id = req.body.user_id;
+  var to_user_id = req.body.to_user_id;
+  var text = req.body.text;
+  if(!room_id || !user_id || !to_user_id || !text){
+    return res.status(400).send("Not enough info for a message private");
+  }
+  db.get().query('UPDATE room SET latest_message = ?, latest_time = ? WHERE id = ?', [text, currentTime.getTime(), room_id], function(err, result){
+    if(err) throw err;
+  });
+  var message = {
+    room_id: room_id,
+    text: text,
+    user_id: user_id,
+    to_user_id: to_user_id,
+    time_stamp: currentTime.getTime()
+  };
+  db.get().query('INSERT INTO message SET ?', [message], function(subErr, subRes){
+    if (subErr) throw subErr;
+    res.status(200).send(subRes);
+  });
+});
+
+//Khi nguoi dung nhan vao Danh Sach Ban Be, mo cua so chat
 app.get('/api/chat/get_room/:user_one/:user_two', function(req, res){
-  getRoomId(req.params.user_one, req.params.user_two, function(result){
+  getPrivateRoom(req.params.user_one, req.params.user_two, function(result){
     res.status(200).send(result[0]);
   });
 });
+//*************************************Private chat*************************************
+
+//####################################################################################################
+
 //-----------Group-----------
 app.get('/api/chat/get_group/:user_id', function(req, res){
   getGroups(req.params.user_id, function(result){
@@ -130,16 +153,14 @@ app.post('/api/chat/create_group', function(req, res) {
   }
   var owner_name = req.body.owner_name;
   var room_name = req.body.room_name;
-  if(!owner_name){
-    return res.status(400).send("You must send owner name");
-  }
-  if(!room_name){
-    return res.status(400).send("You must send room name");
+  if(!owner_name || !room_name){
+    return res.status(400).send("Not enough for create a group");
   }
   var latest_message = owner_name + " create group " + room_name;
   room = {
       name: room_name,
       created_by: arrUser[0],
+      is_private: 0,
       owner_name: owner_name,
       latest_message: latest_message,
       latest_time: currentTime.getTime()
